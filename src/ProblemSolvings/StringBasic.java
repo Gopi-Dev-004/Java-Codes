f8ad7c181674b148f9fe868ffa0ecab3df5d008f
Gopi <mr.gopi0004@gmail.com>
2025-12-28 22:23:55 +0530

    feat: problem soving -Valid Parentheses

--- src/ProblemSolvings/StringBasic.java
+++ src/ProblemSolvings/StringBasic.java
@@ -2,15 +2,16 @@ package ProblemSolvings;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Stack;
 
 public class StringBasic {
 
     //reverse string
-    public static void revStr(){
+    public static void revStr() {
         String str = "Programming knowledge";
 
         StringBuilder sb = new StringBuilder();
-        for(int i=str.length()-1; i>=0; i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
         }
 
@@ -18,33 +19,33 @@ public class StringBasic {
     }
 
     //first non repeating character
-    public static void findNonRepeatingChar(){
+    public static void findNonRepeatingChar() {
         String str = "programming";
 
-        HashMap<Character,Integer> hm = new  HashMap<>();
+        HashMap<Character, Integer> hm = new HashMap<>();
 
-        for(int i=0; i<str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            hm.put(c,hm.getOrDefault(c,0)+1);
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
 
-        for(var sets : hm.entrySet()){
+        for (var sets : hm.entrySet()) {
 
-            if(sets.getValue()==1){
+            if (sets.getValue() == 1) {
 //                System.out.println(sets.getKey()+" "+sets.getValue());
-                System.out.print(sets.getKey()+" ");
+                System.out.print(sets.getKey() + " ");
             }
         }
 
     }
 
     //remove all duplicate
-    public static void removeDuplicate(){
+    public static void removeDuplicate() {
         String str = "programming";
-        StringBuilder  sb = new StringBuilder();
-        for(char c : str.toCharArray()){
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
 
-            if(!(sb.toString().contains(c+""))){
+            if (!(sb.toString().contains(c + ""))) {
                 sb.append(c);
             }
         }
@@ -52,18 +53,18 @@ public class StringBasic {
     }
 
     // largest substring without repeating
-    public static int logestSubWithoutRep(String str){
+    public static int logestSubWithoutRep(String str) {
 
         int start = 0;
         int maxLen = 0;
 
         HashSet<Character> set = new HashSet<>();
 
-        for(int end=0; end<str.length(); end++){
+        for (int end = 0; end < str.length(); end++) {
 
             char currentChar = str.charAt(end);
 
-            while(set.contains(currentChar)){
+            while (set.contains(currentChar)) {
                 set.remove(str.charAt(start));
                 start++;
             }
@@ -75,9 +76,42 @@ public class StringBasic {
     }
 
 
+    public static boolean isValid(String s) {
+
+        Stack b = new Stack();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                b.push(c);
+            } else {
+
+                char b1 = (char) b.peek();
+
+                if ( b1 =='(' && c == ')') {
+                    b.pop();
+                }  else if ( b1== '[' && c == ']') {
+                    b.pop();
+                } else if  ( b1== '{' && c == '}') {
+                    b.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (b.empty())
+            return true;
+        return false;
+
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println(logestSubWithoutRep("abcabcabcdd"));
+        System.out.println(isValid("()"));
+
+//        System.out.println(logestSubWithoutRep("abcabcabcdd"));
 //        removeDuplicate();
 //        findNonRepeatingChar();
 //        revStr();
